1418a520d381138292847a98ee85d0a09dc1e7ed
Nik-Josephson <nikolaus.josephson@maine.edu>
2018-01-24 11:18:47 -0500

    Revising homework1b

--- Homework1/Homework1b.c
+++ Homework1/Homework1b.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	printf("    .--._____,\n .-='=='==-, '\n(O_o_o_o_o_O)\n");
+	printf("\n    .--._____,\n .-='=='==-, '\n(O_o_o_o_o_O)\nSomeone order a tank?\n\n");
 	return 0;
 }
