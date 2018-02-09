3607e156c368cf2e0a97376c2b0c9068501bf522
Nik-Josephson <36248881+Nik-Josephson@users.noreply.github.com>
2018-02-09 11:43:36 -0500

    Update Homework1b.c

--- Homework1/Homework1b.c
+++ Homework1/Homework1b.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main()
 {
+	//Prints a tank into terminal, along with a short response
 	printf("\n    .--._____,\n .-='=='==-, '\n(O_o_o_o_o_O)\nSomeone order a tank?\n\n");
+	//Exits
 	return 0;
 }
