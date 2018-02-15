711f0706585c3b87f6e925a8ca62750692e2e43f
Nik-Josephson <nikolaus.josephson@maine.edu>
2018-02-15 16:10:15 -0500

    Updating Homework2

--- Homework2/Homework2.c
+++ Homework2/Homework2.c
@@ -8,23 +8,50 @@ void program_alarm(int sig);
 int main()
 {
 	FILE *fp;
+	FILE *fd;
+	int junk = 0;
+	int memfree = 0;
+	int alarm_count = 0;
+
 	filename="output.csv";
+	file_meminfo="/proc/meminfo";
+	file_loadavg="/proc/loadavg";
+
 	fp=fopen(filename,"w+");
 
 	printf("Creating and opening output.csv\n");
 
 	fprintf(fp,"Index Num, Free Memory, 1min Load Avg, 5min Load Avg, 10min Load Avg, Processes Running, Total Processes");
-
 	
+	fclose(fp);
 
 	signal(SIGALRM, program_alarm);
 	alarm(1);
-	while(1) sleep(20);
+	while(1) sleep(10);
 
 }
 
 void program_alarm(int sig)
 {
-	printf("Got an alarm\n");
+	printf("Collecting data...\n");
+
+	fd=fopen(file_meminfo,"rt");
+	fscanf(fd, "MemTotal: %d MemFree:  %d", junk, memfree);
+	fclose(fd);
+
+
+	fopen(file_loadavg, "rt");
+	fscanf(fd,"",);
+	fclose(fd);
+
+
+	fp=fopen(filename,"w+");
+	fprintf(fp,"\n %d %d %d %d %d %d %d", alarm_count, memfree, LAone, LAfice, LAten, Prun, Ptotal);
+	fclose(fp);
+
+
+	alarm_count =+1;
 	alarm(1);
+
+	printf("Data Collection Complete!\n");
 }
